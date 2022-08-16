ed2c13117fac66eda4493d4ae62006f8cc68a196
李東穎 <dean19990226@gmail.com>
2022-08-16 10:53:00 +0800

    Update 2022-08-16T02:52:58.454Z

---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.btn__buger').on('click', function (e) {
+    $('.btn__buger').toggleClass('open');
+    $('.nav__content').toggleClass('open');
+  });
+});
+//# sourceMappingURL=all.js.map
