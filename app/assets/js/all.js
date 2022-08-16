c1cbe95cb5a651fe02c96151255cd46b94381758
李東穎 <dean19990226@gmail.com>
2022-08-16 22:32:45 +0800

    首頁一部分

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
 
   $('.btn__buger').on('click',function(e){
     $('.btn__buger').toggleClass('open');
-    $('.nav__content').toggleClass('open');
+    $('.nav__content--mobile').toggleClass('open');
   })
 
 })
