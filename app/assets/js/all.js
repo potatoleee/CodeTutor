de63921541ab31e3ad63debfd3a7b13b02c3f87e
李東穎 <dean19990226@gmail.com>
2022-08-18 01:13:03 +0800

    swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -9,3 +9,15 @@ $(document).ready(() => {
   })
 
 })
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
