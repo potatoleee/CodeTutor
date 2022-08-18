55763d3a999c5db3e7cb3961126c4d2c9476fca1
李東穎 <dean19990226@gmail.com>
2022-08-18 14:14:03 +0800

    Update 2022-08-18T06:13:58.115Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
   });
 });
 var swiper = new Swiper(".mySwiper", {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true
